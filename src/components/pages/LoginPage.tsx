8a9f2fd44992c00bf6aab595cf8c371fa729109f
teachLF <S3904844@mkhb.moe.gov.sa>
2026-09-05 16:47:16 +0300

    Apply local proposal: عدل لي لون زر " تسجيل دخول بحساب مايكروسوفت "

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -225,10 +225,10 @@ export function LoginPage() {
             onClick={signInWithMicrosoft}
           >
             <svg className="ml-2 h-5 w-5" viewBox="0 0 23 23" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-              <rect x="1" y="1" width="10" height="10" fill="#F25022" />
-              <rect x="12" y="1" width="10" height="10" fill="#7FBA00" />
-              <rect x="1" y="12" width="10" height="10" fill="#00A4EF" />
-              <rect x="12" y="12" width="10" height="10" fill="#FFB900" />
+              <rect x="1" y="1" width="10" height="10" fill="red" />
+              <rect x="12" y="1" width="10" height="10" fill="red" />
+              <rect x="1" y="12" width="10" height="10" fill="red" />
+              <rect x="12" y="12" width="10" height="10" fill="red" />
             </svg>
             الدخول بحساب مايكروسوفت (حساب المدرسة)
           </Button>
